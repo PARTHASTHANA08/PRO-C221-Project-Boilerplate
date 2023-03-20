f1ddbfbc8227764b8d6d92476f9d89e175a11db1
PARTHASTHANA08 <73220883+PARTHASTHANA08@users.noreply.github.com>
2023-03-20 19:44:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+var nodemailer = require("nodemailer")
+const transporter = nodemailer.createTransport({
+    port : 465,
+    host : "smtp.gmail.com",
+    auth : {
+        user : "asthanavs19@gmail.com" ,
+        pass : "dnhgpmkydpktrlak"
+    },
+   secure: true
+})
 app.use(express.json())
 
 server.listen(process.env.PORT || 3030);
